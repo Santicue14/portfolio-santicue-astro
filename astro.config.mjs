e51421bb080f327f7cb8295a805ceaa285c0e8e4
Santiago Ariel Baldini Cuevas <santiagobcuevas14@gmail.com>
2024-10-25 12:43:52 -0300

    Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {enabled: true}
+  }),
   integrations: [tailwind()]
 });
